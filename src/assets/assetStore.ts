a812fc2faea1435e46d6bbb4b6739029b4753968
y8ncastle <y8ncastle@kakao.com>
2024-03-02 23:30:20 +0900

    프로필 영역 작업 추가

--- src/assets/assetStore.ts
+++ src/assets/assetStore.ts
@@ -1,7 +1,12 @@
+// icons
+import cvDown from "./icons/cvDown.svg";
+import scrollDown from "./icons/scrollDown.svg";
+
 // images
 import loading from "./images/loading.gif";
+import profile from "./images/profile.png";
 
 // videos
 import background from "./videos/background.mp4";
 
-export { background, loading };
+export { background, cvDown, loading, profile, scrollDown };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,5 @@
-import { background } from "assets/assetStore";
+import { background, cvDown, profile, scrollDown } from "assets/assetStore";
+import { Img } from "components/Image";
 import { useTranslation } from "react-i18next";
 
 const About = () => {
@@ -15,6 +16,40 @@ const About = () => {
           <span>{t(`about.background.titleBelow`)}</span>
         </p>
       </div>
+
+      <div className="scroll-down">
+        {/* 아래로 내리며 살펴보기 */}
+        <p>{t(`about.scrollDown`)}</p>
+        <Img src={scrollDown} width={24} height={24} />
+      </div>
+
+      <section>
+        <div className="profile">
+          <div className="left-side">
+            <p>{t(`about.profile.role`)}</p>
+            <p className="russo">Alec J</p>
+            <p>
+              {t(`about.profile.introPrev`)}
+              <span>{t(`about.profile.name`)}</span>
+              {t(`about.profile.introNext`)}
+            </p>
+          </div>
+
+          <div className="right-side">
+            <Img src={profile} width={505} height={612} />
+
+            <span className="str1">Frontend</span>
+            <span className="str2">Blockchain</span>
+            <span className="str3">Music</span>
+            <span className="str4">INFJ</span>
+
+            <div className="cv-box">
+              <span>CV</span>
+              <Img src={cvDown} width={28} height={28} />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
